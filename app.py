579607242effbdef35ed6ebc3fd1f4aed89f4364
V Anand Shankar <vanandshankar@yahoo.com>
2019-11-29 13:56:10 -0800

    Remove invalid imports

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import altair as alt
-from src import wrangle as wr
 
 from vega_datasets import data
 
